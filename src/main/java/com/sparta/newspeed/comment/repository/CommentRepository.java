802c931010193dde70aaff6103f95d7e42a4727d
HyeonjinChoi <eaglushyeonjin@gmail.com>
2024-06-05 22:11:42 +0900

    fix: 어노테이션 및 메서드 이름 오타 수정

--- src/main/java/com/sparta/newspeed/comment/repository/CommentRepository.java
+++ src/main/java/com/sparta/newspeed/comment/repository/CommentRepository.java
@@ -8,5 +8,5 @@ import java.util.List;
 
 @Repository
 public interface CommentRepository extends JpaRepository<Comment, Long> {
-    List<Comment> findByNewsfeedSeq(long newsfeedSeq);
+    List<Comment> findByNewsfeedNewsFeedSeq(long newsfeedSeq);
 }
