9c83c21a2719628c5cdc6249caa451b14038bc99
GaEun1216 <gani6382@gmail.com>
2024-06-07 13:05:46 +0900

    fix : UserDetailsServiceImpl - 탈퇴 처리된 회윈의 경우 접속 불가

--- src/main/java/com/sparta/newspeed/security/service/UserDetailsServiceImpl.java
+++ src/main/java/com/sparta/newspeed/security/service/UserDetailsServiceImpl.java
@@ -1,6 +1,7 @@
 package com.sparta.newspeed.security.service;
 
 import com.sparta.newspeed.user.entity.User;
+import com.sparta.newspeed.user.entity.UserRoleEnum;
 import com.sparta.newspeed.user.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -18,6 +19,11 @@ public class UserDetailsServiceImpl implements UserDetailsService {
     public UserDetails loadUserByUsername(String userId) throws UsernameNotFoundException {
         User user = userRepository.findByUserId(userId).orElseThrow(() ->
                 new UsernameNotFoundException("Not Found " + userId));
+
+        // 이미 탈퇴 처리된 회원일 경우
+        if (user.getRole() == UserRoleEnum.WITHDRAW) {
+            throw new UsernameNotFoundException("User has withdrawn " + userId);
+        }
 
         return new UserDetailsImpl(user);
     }
