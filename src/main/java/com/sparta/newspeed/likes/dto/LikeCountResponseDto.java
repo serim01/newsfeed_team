6235cd8b3bcb505fab3b93aae86831355b0d684a
GaEun1216 <gani6382@gmail.com>
2024-06-10 18:14:11 +0900

    fix : getter 추가

--- src/main/java/com/sparta/newspeed/likes/dto/LikeCountResponseDto.java
+++ src/main/java/com/sparta/newspeed/likes/dto/LikeCountResponseDto.java
@@ -1,7 +1,9 @@
 package com.sparta.newspeed.likes.dto;
 
 import lombok.AllArgsConstructor;
+import lombok.Getter;
 
+@Getter
 @AllArgsConstructor
 public class LikeCountResponseDto {
     private int likeCount;
